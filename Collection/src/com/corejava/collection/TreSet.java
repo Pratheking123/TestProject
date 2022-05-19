0c8f7d6d63670ae7ed6e686444eb272603510a04
Asus <Asus@DESKTOP-P3MS617>
2022-05-19 17:36:02 +0530

    Include TreeSet Program

---
+++ Collection/src/com/corejava/collection/TreSet.java
@@ -0,0 +1,9 @@
+package com.corejava.collection;
+import java.util.*;  
+public class TreSet {
+public static void main(String[] args) {
+	TreeSet<String> al=new TreeSet<String>();  
+al.add("prathamesh");
+ System.out.println(al);
+}
+}
